bdab212a10e8ba9a44e267fa4238dfb3dc5d0b98
Hamed + <box.technology.net@gmail.com>
2023-08-31 12:11:07 +0330

    visdom remove

--- utils.py
+++ utils.py
@@ -9,38 +9,38 @@ from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 import torch
-import visdom
+#import visdom
 import numpy as np
 from matplotlib import pyplot as plt
 from skactiveml.visualization import plot_decision_boundary, plot_utilities
 
 
-class Visualizer(object):
-    def __init__(self, env='default', **kwargs):
-        self.vis = visdom.Visdom(env=env, **kwargs)
-        self.index = {}
+#class Visualizer(object):
+#    def __init__(self, env='default', **kwargs):
+#        self.vis = visdom.Visdom(env=env, **kwargs)
+#        self.index = {}
 
-    def plot_lines(self, name, y, **kwargs):
-        '''
-        self.plot('loss', 1.00)
-        '''
-        x = self.index.get(name, 0)
-        self.vis.line(Y=np.array([y]), X=np.array([x]),
-                      win=str(name),
-                      opts=dict(title=name),
-                      update=None if x == 0 else 'append',
-                      **kwargs
-                      )
-        self.index[name] = x + 1
-    def disp_image(self, name, img):
-        self.vis.image(img=img, win=name, opts=dict(title=name))
-    def lines(self, name, line, X=None):
-        if X is None:
-            self.vis.line(Y=line, win=name)
-        else:
-            self.vis.line(X=X, Y=line, win=name)
-    def scatter(self, name, data):
-        self.vis.scatter(X=data, win=name)
+#    def plot_lines(self, name, y, **kwargs):
+#        '''
+#        self.plot('loss', 1.00)
+#        '''
+#        x = self.index.get(name, 0)
+#        self.vis.line(Y=np.array([y]), X=np.array([x]),
+#                      win=str(name),
+#                      opts=dict(title=name),
+#                      update=None if x == 0 else 'append',
+#                      **kwargs
+#                      )
+#        self.index[name] = x + 1
+#    def disp_image(self, name, img):
+#        self.vis.image(img=img, win=name, opts=dict(title=name))
+#    def lines(self, name, line, X=None):
+#        if X is None:
+#            self.vis.line(Y=line, win=name)
+#        else:
+#            self.vis.line(X=X, Y=line, win=name)
+#    def scatter(self, name, data):
+#        self.vis.scatter(X=data, win=name)
 
 # def process_feat(feat, length):
 #     new_feat = np.zeros((length, feat.shape[1])).astype(np.float32)
